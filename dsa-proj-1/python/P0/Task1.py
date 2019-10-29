5551df33b3edc7a30dd42070ae51354d9ab5bb77
Mike Thornton <thornton.michaelp@gmail.com>
2019-10-28 20:10:29 -0700

    Update Task1.py

--- dsa-proj-1/python/P0/Task1.py
+++ dsa-proj-1/python/P0/Task1.py
@@ -11,14 +11,26 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
+def datetime(str):
+	[date, time] = str.split(' ')
+	[MM,dd,yyyy] = date.split('-')
+	[hh,mm,ss] = time.split(':')
+	return(dd,MM,yyyy,hh,mm,ss)
+
 def phoneCall(data):
 	return {
 		'sender': data[0],
 		'recipient': data[1],
-		'date': data[2],
+		'date': datetime(data[2]),
 		'duration': int(data[3]),
 	}
 
+def textMessage(data):
+	return {
+		'sender': data[0],
+		'recipient': data[1],
+		'datetime': datetime(data[2]),
+	}
 """
 TASK 1:
 How many different telephone numbers are there in the records? 
@@ -32,8 +44,14 @@ for callData in calls:
 	phonenumbers[call["sender"]] = 1
 	phonenumbers[call["recipient"]] = 1
 
+for data in texts:
+	text = textMessage(data)
+	phonenumbers[text["sender"]] = 1
+	phonenumbers[text["recipient"]] = 1
+
+
+
 print(f"There are {len(phonenumbers)} different telephone numbers in the records.")
 
 
 # Runtime complexity (Big-O): O(n)
-# 		Accessing each input element once
